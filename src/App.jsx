53af7de12c9c64ab197fa38feca7b97c9483c8d0
sannya07 <sanyakansal07@gmail.com>
2025-02-17 14:50:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>This is new EC2 Project...</h1>
+      <h2>Launch...</h2>
+
+      <h3>Sanya Kansal</h3>
+      <h4>2210990793</h4>
+    </>
+  )
+}
+
+export default App
